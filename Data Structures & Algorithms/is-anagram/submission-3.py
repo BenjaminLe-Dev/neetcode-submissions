429ca34372cc26f2f181799f78107c8788550414
BenjaminLe-Dev <benjaminle.dev@gmail.com>
2026-05-22 14:57:39 -0700

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+         sNums = {}
+         tNums = {}
+         for i in s: 
+            #check to see if n already in -> increase += 1
+            #if not then add it w/ sNums[i] = 1
+            if i in sNums:
+                sNums[i] += 1
+            else: 
+                sNums[i] = 1
+        
+         for i in t:
+            if i in tNums:
+                tNums[i] += 1
+            else:
+                tNums[i] = 1
+         return sNums == tNums
+
+
+
+
+        
+        
