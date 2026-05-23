c63bc1e3e3fc0006f5015164ce4a85ddba253470
BenjaminLe-Dev <benjaminle.dev@gmail.com>
2026-05-23 14:47:35 -0700

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,33 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        hashMap = defaultdict(list)
+
+        for i in strs:
+            #we can create an array that keeps track of letter frequency
+            arr = [0] * 26
+            for c in i: 
+                #using ASCII values we can subtract to update an index in our array
+                diff = ord(c) - ord('a')
+                arr[diff] += 1
+            #print(arr)
+            #can't use this array as a key but we can do tuples
+            key = tuple(arr)
+            #if this specific array/tuple is in there already then add the string to our list values
+            if key in hashMap:
+                hashMap[key].append(i)
+            else: 
+            #if not, create one
+                hashMap[key] = [i]
+            #print(hashMap.values())
+            #needs to convert back to list since it's a dict object and the problem is expecting list instead
+        return list(hashMap.values())
+
+
+
+            
+        
+
+
+        
+        
