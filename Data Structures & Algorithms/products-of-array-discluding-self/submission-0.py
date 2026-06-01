40de410908b3ca0f628cbb89977429c693c25480
BenjaminLe-Dev <benjaminle.dev@gmail.com>
2026-06-01 15:07:38 -0700

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        solution = [1] * len(nums)
+        prefix = 1
+
+        for i in range(len(nums)):
+            solution[i] = prefix
+            prefix *= nums[i]
+        
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1):
+            #Multiply both the prefix * postfix
+            solution[i] *= postfix
+            postfix *= nums[i]
+        return solution
+
+
+
+
+
+
+        
